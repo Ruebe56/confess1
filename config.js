486ad1c157cae8bcffed25ae69f6fcff4a53292d
Ruebe56 <125778597+Ruebe56@users.noreply.github.com>
2023-07-18 12:45:52 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,20 @@
+const CONFIG = {
+    titleWeb: "Web Title",
+    introTitle: 'Ruebe',
+    introDesc: `The Earth is strange
+    But why do you keep getting lost
+    Wandering into my lonely heart
+    Behind the word "love" is "affection"`,
+    btnIntro: '^^HiHi^^',
+    title: 'Did you fall in love at first sight? 🥰',
+    desc: 'Did you get intoxicated by that smile?',
+    btnYes: 'I really like you <33',
+    btnNo: 'No, thanks :3',
+    question: 'With over 7 billion people in the world, why do you love me? <3',
+    btnReply: 'Send to you <3',
+    reply: 'Love if you love, don\'t love if you don\'t <33333333',
+    mess: 'I know 🥰. Love you so much 😘😘',
+    messDesc: 'Tonight at 7 PM, I will come to pick you up for a date.',
+    btnAccept: 'Okayyyy <3',
+    messLink: 'https://github.com/zukahai/Confess-Crush' //your message link. Example: https://www.facebook.com/messages/t/100014188333536
+}
